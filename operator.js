fe7284592f0c03e68fb2426644b4b67690aebf1b
mulyana24 <49643139+mulyana24@users.noreply.github.com>
2022-04-06 13:41:56 +0700

    add aritmatik, logic, string, typeof, conditional

---
+++ operator.js
@@ -0,0 +1,22 @@
+//! Aritmatik
+//* +,-,*,/ & % (sisa bagi)
+
+//! Operator Perbandingan 
+// Operator     |     keterangan
+//* ==           |     Sama dengan
+//* !=           |     tidak sama dengan
+//* ===          |     strict sama dengan
+//* !==          |     strict tidak sama dengan
+//* >            |     lebih besar dari
+//* <            |     lebih kecil dari
+//* >=           |     lebih besar sama dengan
+//* <=           |     lebih kecil sama dengan
+
+//! Operator logika
+// Operator        |      Keterangan
+//* &&              |      AND
+//* ||              |      OR
+//* !               |      NOT
+
+//! Operator String
+//* +
