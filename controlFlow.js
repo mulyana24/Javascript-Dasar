9f45ce3cf1127de7f826947628275fbcfa60b7a2
mulyana24 <mulyana240498@gmail.com>
2022-05-30 07:41:13 +0700

    create controlFlow

---
+++ controlFlow.js
@@ -0,0 +1,15 @@
+// apabila program kita memiliki lebih dari satu statement, maka statement tersebut akan dieksekusi dari atas ke bawah atau dari kiri ke kanan
+
+// example
+
+// pengulangan dan pengkondisian
+alert("mulai")
+for (let index = 0; index < 5; index++) {
+    alert("hello world")
+    
+}
+alert("selesai")
+
+
+// var angka = prompt("Masukan angka : ")
+// alert(`Angka yang anda masukan adalah : ${angka}`)
