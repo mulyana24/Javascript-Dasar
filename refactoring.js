83507cd2e84cad47c08fc6bf00e908799806f596
mulyana24 <49643139+mulyana24@users.noreply.github.com>
2022-10-11 05:28:25 +0700

    refactoring

---
+++ refactoring.js
@@ -0,0 +1,12 @@
+function jumlahVolumeDuaKubus(a, b) {
+  //   var volumeA;
+  //   var volumeB;
+  //   var total;
+  //   volumeA = a * a * a;
+  //   volumeB = b * b * b;
+  //   total = volumeA + volumeB;
+
+  return a * a * a + b * b * b;
+}
+
+alert(jumlahVolumeDuaKubus(8, 3));
